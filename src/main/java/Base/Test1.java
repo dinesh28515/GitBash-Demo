cc119091f4a5baddd4cd35463f56e1996156cb1f
DD <dinesh28515@gmail.com>
2022-01-13 19:10:45 +0530

    this is commit from dinesh

--- src/main/java/Base/Test1.java
+++ src/main/java/Base/Test1.java
@@ -3,6 +3,8 @@ package Base;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("java");
+        System.out.println("java1");
+        System.out.println("java2");
     }
 
 }
