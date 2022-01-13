cbdf981039a416105f7cca18783ed9ce0b747eb2
DD <dinesh28515@gmail.com>
2022-01-13 19:36:16 +0530

    develop branch

--- src/main/java/Base/Test1.java
+++ src/main/java/Base/Test1.java
@@ -5,6 +5,18 @@ public class Test1 {
         System.out.println("java");
         System.out.println("java1");
         System.out.println("java2");
+        Test1 t1=new Test1();
+        t1.m1();
+        t1.m2();
+        
+    }
+    public void m1()
+    {
+        System.out.println("m1");
+    }
+    public void m2()
+    {
+        System.out.println("m2");
     }
 
 }
